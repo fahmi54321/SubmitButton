aa195e00c6c6dc61156927ba85abac74f7a8c777
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-02 14:16:03 +0700

    submit button

--- app/src/main/java/com/android/a17submitbutton/MainActivity.java
+++ app/src/main/java/com/android/a17submitbutton/MainActivity.java
@@ -3,12 +3,27 @@ package com.android.a17submitbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.spark.submitbutton.SubmitButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    SubmitButton submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Submit", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
